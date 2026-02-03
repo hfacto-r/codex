6e37f0425f5481a00aa3cd0d1543862e47e32bd7
hfactor <hk4physics@gmail.com>
2026-02-03 20:41:11 +0530

    code: min value of a list in O(n) and O(n^2)

--- algo_book/chapter_2/min_list.py
+++ algo_book/chapter_2/min_list.py
@@ -1,18 +1,24 @@
 lst = [2, 5, 1, 7, 8, 9]
+#O(n) Function
 def minval(lst):
     min = lst[0]
-    for i in range(len(lst)):
-        if lst[i] < min:
-            min = lst[i]
+    for i in lst:
+        if i < min:
+            min = i
     return min
 
-
+#O(n^2) Function.
+#else can be used with for. else block runs only when loop completed without breaking.
 def minval2(lst):
-    min = lst[0]
+    min = None
     for i in lst:
         for j in lst:
-            if i<min:
-                min = i
-    return min
+            if i>j:
+                break
+        else:
+            min = i
+    return min   
+            
+            
 
 print(minval2(lst))
