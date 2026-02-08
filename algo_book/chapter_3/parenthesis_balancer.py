38bf5e6cbba8cbb0975453e326c55445a8531a33
hfactor <hk4physics@gmail.com>
2026-02-08 08:31:59 +0530

    feat: add alternate implementation

--- algo_book/chapter_3/parenthesis_balancer.py
+++ algo_book/chapter_3/parenthesis_balancer.py
@@ -7,9 +7,28 @@ def checker(astr):
         if char == '(':
             s.push(char)
         elif char == ')':
-            s.pop()
+            if s.is_empty():
+                return False
+            else:
+                s.pop()
         else:
             pass
     return (s.is_empty())
 
-print(checker('((())'))
+def checker_alt(astr):
+    s= Stack()
+    pos = 0
+    while pos < len(astr): 
+        symbol = astr[pos]
+        if symbol == '(':
+            s.push(symbol)
+        else:
+            if s.is_empty():
+                return False
+            else:
+                s.pop()
+        pos += 1
+    return (s.is_empty())
+
+print(checker_alt('()()())'))
+            
