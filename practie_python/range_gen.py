a70155fb2df105869c66d924f8c8d3daade1aeda
hfactor <hk4physics@gmail.com>
2026-02-08 17:00:58 +0530

    feat: add negative step in range

--- practie_python/range_gen.py
+++ practie_python/range_gen.py
@@ -1,7 +1,27 @@
 class Range:
+    """Implementation of Python Range function"""
     def __init__(self, start, end, step=1):
         self.start = start
         self.end = end
         self.step = step
 
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.step > 0:
+            if self.start >= self.end:
+                raise StopIteration
+            else:
+                temp = self.start
+                self.start += self.step
+                return temp
+        elif self.step < 0:
+            if self.start<= self.end:
+                raise StopIteration
+            else:
+                temp = self.start
+                self.start += self.step
+                return temp
+            
 
