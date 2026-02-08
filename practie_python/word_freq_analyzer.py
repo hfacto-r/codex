fd7ede9e5056fd585f825bab7848c7dd43898e93
hfactor <hk4physics@gmail.com>
2026-02-08 10:56:18 +0530

    chore: add solution steps

---
+++ practie_python/word_freq_analyzer.py
@@ -0,0 +1,11 @@
+#Word Frequency Analyzer
+
+"""Steps:
+    1. Take a text file. Iterate through the lines.convert the line into a list words (temporarily save)
+    2. If a word occure for the first time , add to dict and assign a val of 1
+    3. If the word occurs again raise the val by one.
+    4. Return a tuple of (word, count). 
+    5. Sort the dict based on the val return a tuple of word, count pair--> Sorted by Count
+    6. Sort the dict based on the key, return a tuple of word count pair--> Sorted alphabetically """
+
+
