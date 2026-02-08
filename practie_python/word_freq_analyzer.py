04a2d84cfb99840f89ba12f453ad84d5902a8390
hfactor <hk4physics@gmail.com>
2026-02-08 11:27:34 +0530

    feat: add word counter

--- practie_python/word_freq_analyzer.py
+++ practie_python/word_freq_analyzer.py
@@ -7,5 +7,20 @@
     4. Return a tuple of (word, count). 
     5. Sort the dict based on the val return a tuple of word, count pair--> Sorted by Count
     6. Sort the dict based on the key, return a tuple of word count pair--> Sorted alphabetically """
+from pathlib import Path
+
+def freq_analyzer(file):
+    cdict = {}
+    path = Path(str(file))
+    with path.open('r') as f:
+        for line in f:
+            wlist = line.strip().lower().split()
+            for word in wlist:
+                cdict[word] = cdict.get(word, 0) + 1
+    result = [(k,v) for k,v in cdict.items()]
+    # add soring methods
+
+
+
 
 
