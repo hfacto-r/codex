1a41848db6bfc3304f4f3024def81dca3b008c65
hfactor <hk4physics@gmail.com>
2026-02-12 20:15:44 +0530

    feat: add a function to check parenthesis are balanced

--- algo_book/chapter_3/stack/to_postfix.py
+++ algo_book/chapter_3/stack/to_postfix.py
@@ -4,6 +4,18 @@ Assumes the operators are limited to +,-,*,/
 """
 from stack import Stack
 
+def par_checker(expr):
+    s = Stack()
+    for char in expr:
+        if char == '(':
+            s.push(char)
+        elif char == ')':
+            if s.is_empty():
+                return False
+            else:
+                s.pop()
+    return (s.is_empty())
+
 def to_postfix(expr):
     s = Stack()
     operands = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -20,6 +32,7 @@ def to_postfix(expr):
 
 def main():
     expr_b = '((A+(B*C))+D)'
+    print(par_checker(expr_b))
     print(to_postfix(expr_b))
 
 if __name__ == '__main__':
