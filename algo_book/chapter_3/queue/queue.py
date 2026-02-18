a5527699b7e793e7d91556465a37b3822395538d
hfactor <hk4physics@gmail.com>
2026-02-18 20:27:59 +0530

    code:implement Queue ADT

---
+++ algo_book/chapter_3/queue/queue.py
@@ -0,0 +1,33 @@
+class Queue:
+    """Implementing a Queue ADT in Python"""
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return (self.items == [])
+
+    def enqueue(self, item):
+        self.items.insert(0,item)
+
+    def dequeue(self):
+        self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+def main():
+    q = Queue()
+    print(q.is_empty())
+    print(q.size())
+    print(q.enqueue(True))
+    print(q.enqueue(8.7))
+    print(q.enqueue('Motion'))
+    print(q.dequeue())
+    print(q.size())
+    print(q.items)
+    print(q.is_empty())
+
+if __name__ == '__main__':
+    main()
+
+
