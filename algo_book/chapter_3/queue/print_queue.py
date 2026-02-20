43912b6cd7599019a08d379f91bf670d43fe92b1
hfactor <hk4physics@gmail.com>
2026-02-20 07:03:08 +0530

    feat: add Task class

--- algo_book/chapter_3/queue/print_queue.py
+++ algo_book/chapter_3/queue/print_queue.py
@@ -1,3 +1,5 @@
+import random
+
 class Printer:
     def __init__(self, ppm):
         self. pagerate = ppm
@@ -16,5 +18,19 @@ class Printer:
     def start_next(self, new_task):
         self.current_task = new_task
         self.time_remaining = self.current_task.get_pages() *60/self.pagerate
+
+class Task:
+    def __init__(self, time):
+        self.timestamp = time
+        self.no_pages = random.randrange(1, 21)
+
+    def get_timestamp(self):
+        return self.timestamp
+
+    def get_pages(self):
+        return self.no_pages
+
+    def wait_time(self, current_time):
+        return current_time - self.timestamp
 
 
