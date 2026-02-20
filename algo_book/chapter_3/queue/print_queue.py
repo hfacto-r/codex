3b4f64d3f97a7c75daea517ce4640d351ef43866
hfactor <hk4physics@gmail.com>
2026-02-20 07:41:28 +0530

    add simulation function

--- algo_book/chapter_3/queue/print_queue.py
+++ algo_book/chapter_3/queue/print_queue.py
@@ -1,4 +1,5 @@
 import random
+from queue import Queue
 
 class Printer:
     def __init__(self, ppm):
@@ -33,4 +34,34 @@ class Task:
     def wait_time(self, current_time):
         return current_time - self.timestamp
 
+def simulation(num_seconds, ppm):
+    labprinter = Printer(ppm)
+    print_queue = Queue()
+    waiting_times = []
+    
+    for current_second in range(num_seconds):
+        if new_print_task():
+            new_task = Task(current_second)
+            print_queue.enqueue(new_task)
 
+        if (not labprinter.busy()) and (not print_queue.is_empty()):
+            new_task = print_queue.dequeue()
+            labprinter.start_next(new_task)
+            waiting_times.append(new_task.wait_time(current_second))
+        labprinter.tick()
+
+    average_wait = sum(waiting_times)/len(waiting_times)
+    print(f'Average waiting time {average_wait} {print_queue.size()} tasks remaining')
+
+
+
+
+def new_print_task():
+    num = random.randrange(1, 181)
+    if num == 180:
+        return True
+    else:
+        return False
+
+for i in range(10):
+    simulation(36000, 5)
